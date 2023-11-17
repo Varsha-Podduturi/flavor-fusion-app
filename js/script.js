0d37b46ced1d6365902b23dab47b6241d5b9bcc1
Varsha Podduturi <varshareddy4017@gmail.com>
2023-11-17 02:14:25 -0500

    added partail layout of add recipe

--- js/script.js
+++ js/script.js
@@ -1,4 +1,27 @@
 document.addEventListener("DOMContentLoaded", function () {
+    const heroContainer = document.querySelector(".hero-inner-container");
+    const logoContainer = document.querySelector(".logo-container");
+    const navbarOuter = document.querySelector('.navbar-outer-container')
+
+    window.addEventListener("scroll", function () {
+        const scrollPosition = window.scrollY;
+
+        // Adjust the hero container
+        if (scrollPosition > 100) {
+            heroContainer.classList.add("sticky-hero");
+            logoContainer.classList.add("scrolled");
+        } else {
+            heroContainer.classList.remove("sticky-hero");
+            logoContainer.classList.remove("scrolled");
+        }
+
+
+    });
+});
+
+
+document.addEventListener("DOMContentLoaded", function () {
+    
     
     const allDropDowns = document.querySelectorAll(".dropdown-btn")
 
